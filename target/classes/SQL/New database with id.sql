3cf1fa3449c566306a5abcc448943ee38e5946d8
Suffredini Matteo <38665632+Suffredini@users.noreply.github.com>
2019-10-21 18:23:01 +0200

    adda sql files

---
+++ target/classes/SQL/New database with id.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS SocialNetwork; 
+CREATE DATABASE SocialNetwork;
+use SocialNetwork;
+
+CREATE TABLE IF NOT EXISTS User (
+	IdUser INT PRIMARY KEY AUTO_INCREMENT,
+    Username CHAR(50) PRIMARY KEY,
+    Password CHAR(64) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Post (
+	IdPost INT PRIMARY KEY AUTO_INCREMENT,
+    strPost CHAR(50) NOT NULL,
+    User INT NOT NULL,
+    Date TIMESTAMP NOT NULL,
+  	FOREIGN KEY(User) REFERENCES User(IdUser)
+		ON DELETE cascade
+        ON UPDATE cascade
+);
+
+CREATE TABLE IF NOT EXISTS Comment (
+	IdComment INT PRIMARY KEY AUTO_INCREMENT,
+    strComment CHAR(50) NOT NULL,
+    User INT NOT NULL,
+    Post INT NOT NULL,
+    Date TIMESTAMP NOT NULL,
+  	FOREIGN KEY(User) REFERENCES User(IdUser)
+		ON DELETE cascade
+        ON UPDATE cascade,
+    FOREIGN KEY(Post) REFERENCES Post(IdPost)
+		ON DELETE cascade
+        ON UPDATE cascade
+);
+
